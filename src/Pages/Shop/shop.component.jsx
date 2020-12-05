d789009718dda9efb6baa86b8ed28c3bf02dbfaa
Brian Jones <bjones256@gmail.com>
2020-12-05 14:12:52 -0500

    moved getting the collection data from inside the shop component into redux. It's now a single get on component did mount vs an open connection with firestore. I would like to see how this would work with an open connection like it was before. may go backand refactor

--- src/Pages/Shop/shop.component.jsx
+++ src/Pages/Shop/shop.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
 import CollectionsOverview from '../../Components/CollectionsOverview/collections-overview.component';
 import CollectionPage from '../Collection/collection.component';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
-import { updateCollections } from '../../redux/shop/shop.actions';
+// import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+// import { updateCollections } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
 
 import WithSpinner from '../../Components/WithSpinner/with-spinner.component';
 
@@ -14,24 +17,31 @@ const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component{
-    state = {
-        loading: true
-    };
+    // state = {
+    //     loading: true
+    // };
     
-    unsubscribeFromSnapshot = null;
+    // unsubscribeFromSnapshot = null;
 
     componentDidMount(){
-        const { updateCollections } = this.props;
-        const collectionRef = firestore.collection('collections');
+const { fetchCollectionsStartAsync } = this.props;
+fetchCollectionsStartAsync()
+
+        // const { updateCollections } = this.props;
+        // const collectionRef = firestore.collection('collections');
         
-        collectionRef.get().then(snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-            updateCollections(collectionsMap);
-            this.setState({
-                loading:false
-            })
-        })
+        // collectionRef.get().then(snapshot => {
+        //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        //     updateCollections(collectionsMap);
+        //     this.setState({
+        //         loading:false
+        //     })
+        // })
+
+        //the above is now moved into into redux
+
         // I currently like the below process better. It keeps the connection open similar to sockets. If I add a product to the backend it automatically updates the front end.
+        
         // collectionRef.onSnapshot( async snapshot => {
         //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
         //     updateCollections(collectionsMap);
@@ -41,14 +51,14 @@ class ShopPage extends React.Component{
         // })
     }
     render(){
-        const { match } = this.props;
-        const { loading } = this.state;
+        const { match, isCollectionsFetching, isCollectionsLoaded } = this.props;
+        // const { loading } = this.state;
         return(
         <div className='shop-page'>
             <Route exact path={`${match.path}`} 
                 render = {(props) => 
                 <CollectionOverviewWithSpinner
-                isLoading={loading}
+                isLoading={isCollectionsFetching}
                 {...props}
                 />
                 }
@@ -56,7 +66,7 @@ class ShopPage extends React.Component{
             <Route path={`${match.path}/:collectionId`} 
                 render = {(props) => 
                 <CollectionPageWithSpinner
-                isLoading={loading}
+                isLoading={!isCollectionsLoaded}
                 {...props}
                 />
                 }
@@ -65,9 +75,13 @@ class ShopPage extends React.Component{
     )}
 }  
 
+const mapStateToProps = createStructuredSelector({
+    isCollectionsFetching: selectIsCollectionFetching,
+    isCollectionsLoaded: selectIsCollectionsLoaded
+});
+
 const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => 
-    dispatch(updateCollections(collectionsMap))
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 })
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
